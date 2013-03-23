6952b1cc67007ce8ed4e2c82e4fa6510d32b92d2
Nick Ruff <nickruff@umich.edu>
2013-03-23 16:23:14 -0400

    added video taker

--- SwiftGif/SwiftGif/SGFirstViewController.h
+++ SwiftGif/SwiftGif/SGFirstViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SGFirstViewController : UIViewController
+@interface SGFirstViewController : UIViewController<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
 @end
--- SwiftGif/SwiftGif/SGFirstViewController.m
+++ SwiftGif/SwiftGif/SGFirstViewController.m
@@ -7,25 +7,85 @@
 //
 
 #import "SGFirstViewController.h"
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface SGFirstViewController ()
-
-@end
 
 @implementation SGFirstViewController
+
+BOOL shouldOpen = true;
+
+- (IBAction)gifClick:(id)sender {
+    [self doCamera];
+}
+
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    shouldOpen = true;
     
     
 }
+
+- (void) doCamera {
+    UIImagePickerController *cameraView = [[UIImagePickerController alloc] init];
+    
+    cameraView.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    //cameraView.mediaTypes = [UIImagePickerController availableMediaTypesForSourceType: UIImagePickerControllerSourceTypeCamera];
+    
+    cameraView.mediaTypes =
+    [[NSArray alloc] initWithObjects: (NSString *) kUTTypeMovie, nil];
+    
+    cameraView.showsCameraControls = YES;
+    
+    cameraView.delegate = self;
+    
+    [self.view addSubview:cameraView.view];
+    [cameraView viewWillAppear:YES];
+    [cameraView viewDidAppear:YES];
+    
+    [self presentViewController:cameraView animated:YES completion:nil];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:YES];
+    
+    /*if (shouldOpen) {
+    
+        [self doCamera];
+        
+    }*/
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)imagePickerController:(UIImagePickerController*)picker
+didFinishPickingMediaWithInfo:(NSDictionary*)info{
+    
+    //[picker UISaveVideoAtPathToSavedPhotosAlbum];
+    shouldOpen = false;
+    [self dismissViewControllerAnimated:NO completion:nil];
+    [picker.view removeFromSuperview];
+    [self.tabBarController setSelectedIndex:1];
+    // instead of above, take to special edit view now
+    
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController*)picker{
+    shouldOpen = false;
+    [self dismissViewControllerAnimated:NO completion:nil];
+    [picker.view removeFromSuperview];
+    //[self.tabBarController setSelectedIndex:1];
+    
 }
 
 @end
