f0523b7a8182ce830ca8e02b57544bc434588ed3
Ting Chen <tingshen.chen@gmail.com>
2013-04-08 20:00:03 -0400

    add new controller

---
+++ SwiftGif/SwiftGif/SGFrameEditController.h
@@ -0,0 +1,13 @@
+//
+//  SGFrameEditController.h
+//  SwiftGif
+//
+//  Created by Tingshen Chen on 4/8/13.
+//  Copyright (c) 2013 Team SwiftGif. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGFrameEditController : UIViewController
+
+@end
---
+++ SwiftGif/SwiftGif/SGFrameEditController.m
@@ -0,0 +1,38 @@
+//
+//  SGFrameEditController.m
+//  SwiftGif
+//
+//  Created by Tingshen Chen on 4/8/13.
+//  Copyright (c) 2013 Team SwiftGif. All rights reserved.
+//
+
+#import "SGFrameEditController.h"
+
+@interface SGFrameEditController ()
+
+@end
+
+@implementation SGFrameEditController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
